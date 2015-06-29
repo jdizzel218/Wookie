71d399383eaa03b5c84bf88a5034d31d1d82bf41
mcranford13 <m.cranford13@gmail.com>
2015-06-29 14:45:51 -0600

    Streamlined and updated.

--- Python/windowspost.py
+++ Python/windowspost.py
@@ -3,23 +3,46 @@ import subprocess
 
 def networkgather():
 	
+	print "========================="
+	print "   Network Information   "
+	print "========================="
+	
 	arp = ("arp", "-a")
 	netview = ("net", "view")
-	netuser = ("net", "user")
+	
 	ipconfig = ("ipconfig", "/all")
 	#nbtstat = (" nbtstat", "-n") For some reason this command isn't being recognized.
 	netstat = ("netstat", "-o")
 	
-	cmdlist = [arp, netview, netuser, ipconfig, netstat]
+	cmdlist = [arp, netview,ipconfig, netstat]
 	
 	
 	for i in cmdlist:
 			subprocess.Popen(i, 0, None, None, None, shell=True).wait() # loop through all the system information gathering commands.
+	print ''
+	 
+def useraccountsenum():
 	
+	print ''
+	print "===================="
+	print "   User Accounts   "
+	print "==================="
+	print ''
 	
-
-
+	netuser = ("net", "user")
+	
+	subprocess.Popen(netuser, 0, None, None, None, shell=True).wait()
+	
+	print ''
+	print ''
+	
 def systeminfogather():
+	
+	print ''
+	print "========================"
+	print "   System Information   "
+	print "========================"
+	print ''
 	
 	systeminfo = ("systeminfo")
 	tasklist= ("tasklist")
@@ -28,10 +51,23 @@ def systeminfogather():
 	
 	for i in cmdlist:
 		subprocess.Popen(i, 0, None, None, None, shell=True).wait()
+	
+	print ''
+	print ''
 
+def endlog():
+	print ""
+	print ""
+	print ""
+	print "================"
+	print "   END OF LOG   "
+	print "================"
 
 def main():
+	useraccountsenum()
 	systeminfogather()
 	networkgather()
+	endlog()
+	
 	
 main()
