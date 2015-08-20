33739b09aab9d74f390f24f78fa84f36aaebe719
mcranford13 <m.cranford13@gmail.com>
2015-08-20 15:08:05 -0600

    Moved Bots folder

---
+++ Bots/Bot Project/Main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Main.py
+#  
+#  Copyright 2015 MCranfor <MCranfor@MCRANFORD-9010>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+
+#################
+#   Libraries   #
+#################
+
+import time
+import pyautogui
+from functions import *
+from classes import *
+
+########################
+#   Global Variables   #
+########################
+
+
+
+
+def main():	
+	while True:
+		mouse = get_mouse_position()
+		print mouse
+	return 
+
+if __name__ == '__main__':
+	main()
+
