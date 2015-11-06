7fe859fa852f138dde0c4de71b83a26f5c17d01f
Matthew Cranford <MCranfor@MCRANFORD-E642.CTAC.1800contacts.com>
2015-11-06 08:31:08 -0700

    finished lab #18

---
+++ C#/Lab_18/Lab_18/Program.cs
@@ -0,0 +1,49 @@
+﻿// File/Project Prolog
+// Name: Matthew Cranford
+// CS 1400 Section 001
+// Project: Lab_18
+// Date: 11/5/2015 8:19 PM
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+/*
+Pseudo-Code:
+
+#1. Ask User if they want to roll the dice.
+        -if No then quit
+#2. Get user response, make sure the input is valid (either a yes, y, ye, or a no, n)
+#3. Simulate a pair of dice by creating a random object and passing it 1 & 6. (i.e random.Next(1,6))
+#4. If it returns 2 6's, then display: "you rolled boxcars", else: Display output amd return back to step one.
+#5. If it returns 2 1's then display: "You rolled snake-eyes" else: display the output and return back to step one.
+#6. default: Display: "You rolled... X & X" <<-- replace with numbers
+#7. Return back to step 1 and ask the user if the want to roll the dice
+
+Do this all in a Do-While loop.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_18
+{
+    class Program
+    {
+        /// <summary>
+        /// Purpose: Main Entry point of program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
