10484a01424d4e457744157a5c77a04c59ec2f5c
mcranford13 <m.cranford13@gmail.com>
2015-12-04 13:08:52 -0700

    Started a new project.

---
+++ C#/BowlingScores1/BowlingScores1/BowlTeam.cs
@@ -0,0 +1,72 @@
+﻿
+/*
+Pseudo-Code:
+
+#1. Take input from usere -> first name and bowling score on single line.
+#2. Use the Split method to parse the input, the name goes in an array of strings, and the score in an array of ints
+#3. If the user enters an empty line, then quit adding users and scores to the arrays.
+#4. Output the users and scores. Output the highest score, the lowest score and the average score.
+
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BowlingScores1
+{
+    class BowlTeam
+    {
+
+        int[] _scores;
+        string[] _names;
+
+        string[] _userInputParsed;
+        public int HigestScore{get;set;} 
+        public int LowestScore { get; set; }
+
+        public int AverageScore { get; set; }
+       
+        public void GetData()
+        {
+            string userInput;
+            do {
+
+                WriteLine("Enter the Name and Score seperated by a comma, i.e -> Frank,86");
+                userInput = ReadLine();
+                _userInputParsed = userInput.Split(',');
+               
+            } while (userInput.Length > 0);
+
+
+
+            for(int i = 0; i < 10; i++)
+            {
+                _names[i] = _userInputParsed[0];
+                _scores[i] = _userInputParsed[1];
+            }
+
+        }
+        static int CalcHighestScore()
+        {
+            return 0;
+        }
+
+        static int CalcLowestScore()
+        {
+            return 0;
+        }
+
+        static int CalcAverageScore()
+        {
+            
+            return 0;
+        }
+
+    }
+}
---
+++ C#/BowlingScores1/BowlingScores1/Program.cs
@@ -0,0 +1,36 @@
+﻿//File Prologue
+
+
+/*
+Pseudo-Code:
+
+#1. Take input from usere -> first name and bowling score on single line.
+#2. Use the Split method to parse the input, the name goes in an array of strings, and the score in an array of ints
+#3. If the user enters an empty line, then quit adding users and scores to the arrays.
+#4. Output the users and scores. Output the highest score, the lowest score and the average score.
+
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScores1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        static void Menu()
+        {
+
+        }
+    }
+}
